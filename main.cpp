a8f7aff69f74e27bfeddfe5e60a8be8952d9cfd2
thanhdxuan <hello.thanhdauxuan@gmail.com>
2022-11-14 22:46:05 +0000

    test ssh key

--- main.cpp
+++ main.cpp
@@ -3,26 +3,50 @@
 
 #include "SortAlgorithms.h"
 // #include "DLinkedList.cpp"
+#include <bits/stdc++.h>
 #include <math.h>
+#include <time.h>
+#include <tree_sitter/parser.h>
 
 #include "BinarySearchTree.h"
 #include "BinaryTree.h"
 #include "DataLog.h"
 #include "SLinkedList.h"
+
+#include "AVLTree.h"
 using namespace std;
 
+void print(const vector<int> &vector) {
+   for (auto i : vector) {
+      cout << i << " ";
+   }
+}
 int main() {
-   int arr1[] = {9, 3, 8, 2, 1, 6, 7, 4, 5};
-   int n = sizeof(arr1) / sizeof(arr1[0]);
-   SLinkedList<int>* l1 = new SLinkedList<int>(arr1, n);
-   l1->printList();
-   l1->bubbleSort();
-   l1->printList();
+   BinarySearchTree<int> BST;
+   int arr[] = {41, 20, 65, 11, 29, 50, 91, 32, 72, 99};
+   int n = sizeof(arr) / sizeof (arr[0]);
+   for (int i = 0; i < n; i++) {
+      BST.add(arr[i]);
+   }
+   vector<int> res = BST.levelAlterTraverse();
+   print(res);
    return 0;
 }
-/* 9
-2     10
-  8       11
 
-2 8 9 10 11
- */
+/*
+    3
+5       2
+  9   1    4
+           5  1
+
+3 + 5 + 19 = 27
+3 + 2 + 1 = 6
+3 + 2 + 4 + 15 = 24
+3 + 2 + 4 + 10 = 19
+
+
+8 + 6 + 9 = 23
+8
+3 6 3 = 12 (-3)
+
+*/
